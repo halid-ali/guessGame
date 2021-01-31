fd55ee69c6fa5ad7dfe15c2f9eba27b9ed8aeeb6
Halid Ali <halid.ali@methodpark.de>
2021-01-31 17:19:38 +0100

    register screen functionality completed

--- lib/screens/user/register.dart
+++ lib/screens/user/register.dart
@@ -4,6 +4,7 @@ import 'package:guessGame/screens/user/photo_upload.dart';
 import 'package:guessGame/widgets/button.dart';
 import 'package:guessGame/widgets/step_indicator.dart';
 import 'package:guessGame/widgets/enter_exit_transition.dart';
+import 'package:guessGame/widgets/textformfield.dart';
 
 class RegisterScreen extends StatefulWidget {
   RegisterScreen({Key key}) : super(key: key);
@@ -14,26 +15,13 @@ class RegisterScreen extends StatefulWidget {
 
 class _RegisterScreenState extends State<RegisterScreen> {
   final _formKey = GlobalKey<FormState>();
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
-  final _passwordAgainController = TextEditingController();
-  final _emailController = TextEditingController();
-
-  Color _usernameSuffixIconColor = Colors.white;
-  Color _passwordSuffixIconColor = Colors.white;
-  Color _passwordAgainSuffixIconColor = Colors.white;
-  Color _emailSuffixIconColor = Colors.white;
+  final _usernameContoller = TextEditingController();
+  final _passwordContoller = TextEditingController();
+  final _passwordRepeatContoller = TextEditingController();
+  final _emailContoller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    _usernameController.addListener(() {
-      setState(() {
-        _usernameSuffixIconColor =
-            _validateUsername(_usernameController.text) == null
-                ? Colors.green
-                : Colors.white;
-      });
-    });
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.pink,
@@ -69,139 +57,61 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       color: Colors.white,
                     ),
                     padding: const EdgeInsets.symmetric(
-                      horizontal: 30.0,
+                      horizontal: 20.0,
                       vertical: 10.0,
                     ),
                     child: SingleChildScrollView(
                       child: Column(
                         children: <Widget>[
-                          TextFormField(
-                            controller: _usernameController,
-                            validator: _validateUsername,
-                            decoration: InputDecoration(
-                              errorMaxLines: 2,
-                              helperText: '* Required(l10n)',
-                              hintText: AppLocalizations.of(context).username,
-                              icon: Icon(
-                                Icons.account_box_rounded,
-                                size: 33,
-                                color: Colors.grey,
-                              ),
-                              suffixIcon: Icon(
-                                Icons.check,
-                                color: _usernameSuffixIconColor,
-                              ),
-                              enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.grey,
-                                ),
-                              ),
-                              focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.pink,
-                                ),
-                              ),
-                            ),
+                          CustomTextFormField(
+                            isRequired: true,
+                            hintText: AppLocalizations.of(context).username,
+                            iconData: Icons.account_box_rounded,
+                            validateFunc: _validateUsername,
+                            controller: _usernameContoller,
                           ),
                           SizedBox(height: 20),
-                          TextField(
-                            controller: _passwordController,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              errorMaxLines: 2,
-                              helperText: '*Required(l10n)',
-                              hintText: AppLocalizations.of(context).password,
-                              icon: Icon(
-                                Icons.lock_rounded,
-                                size: 33,
-                                color: Colors.grey,
-                              ),
-                              suffixIcon: Icon(
-                                Icons.check,
-                                color: _passwordSuffixIconColor,
-                              ),
-                              enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.grey,
-                                ),
-                              ),
-                              focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.pink,
-                                ),
-                              ),
-                            ),
+                          CustomTextFormField(
+                            isRequired: true,
+                            isObscureText: true,
+                            errorLines: 5,
+                            hintText: AppLocalizations.of(context).password,
+                            iconData: Icons.lock_rounded,
+                            validateFunc: _validatePassword,
+                            controller: _passwordContoller,
                           ),
                           SizedBox(height: 20),
-                          TextField(
-                            controller: _passwordAgainController,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              errorMaxLines: 2,
-                              helperText: '*Required(l10n)',
-                              hintText:
-                                  AppLocalizations.of(context).repeat_password,
-                              icon: Icon(
-                                Icons.lock_rounded,
-                                size: 33,
-                                color: Colors.grey,
-                              ),
-                              suffixIcon: Icon(
-                                Icons.check,
-                                color: _passwordAgainSuffixIconColor,
-                              ),
-                              enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.grey,
-                                ),
-                              ),
-                              focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.pink,
-                                ),
-                              ),
-                            ),
+                          CustomTextFormField(
+                            isRequired: true,
+                            isObscureText: true,
+                            hintText:
+                                AppLocalizations.of(context).repeat_password,
+                            iconData: Icons.lock_rounded,
+                            validateFunc: _validatePasswordRepeat,
+                            controller: _passwordRepeatContoller,
                           ),
                           SizedBox(height: 20),
-                          TextField(
-                            controller: _emailController,
-                            decoration: InputDecoration(
-                              errorMaxLines: 2,
-                              helperText: '',
-                              hintText: AppLocalizations.of(context).email,
-                              icon: Icon(
-                                Icons.email_rounded,
-                                size: 33,
-                                color: Colors.grey,
-                              ),
-                              suffixIcon: Icon(
-                                Icons.check,
-                                color: _emailSuffixIconColor,
-                              ),
-                              enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.grey,
-                                ),
-                              ),
-                              focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.pink,
-                                ),
-                              ),
-                            ),
+                          CustomTextFormField(
+                            isRequired: true,
+                            hintText: AppLocalizations.of(context).email,
+                            iconData: Icons.mail_rounded,
+                            validateFunc: _validateMail,
+                            controller: _emailContoller,
                           ),
                           SizedBox(height: 20.0),
                           CustomButton(
                             color: Color(0xFF56CD4D),
                             text: AppLocalizations.of(context).next,
                             func: () {
-                              Navigator.push(
-                                context,
-                                EnterExitRouteTransition(
-                                  exitPage: widget,
-                                  enterPage: PhotoUpload(),
-                                ),
-                              );
+                              if (_formKey.currentState.validate()) {
+                                Navigator.push(
+                                  context,
+                                  EnterExitRouteTransition(
+                                    exitPage: widget,
+                                    enterPage: PhotoUpload(),
+                                  ),
+                                );
+                              }
                             },
                             icon: Icons.arrow_forward_ios,
                           ),
@@ -218,15 +128,6 @@ class _RegisterScreenState extends State<RegisterScreen> {
     );
   }
 
-  @override
-  void dispose() {
-    _usernameController.dispose();
-    _passwordController.dispose();
-    _passwordAgainController.dispose();
-    _emailController.dispose();
-    super.dispose();
-  }
-
   String _validateUsername(String username) {
     if (username == null || username.isEmpty) {
       return 'Username cannot be null.(l10n)';
@@ -234,6 +135,60 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
     if (username.length < 5 || username.length > 15) {
       return 'Username length must be between 5 and 15.(l10n)';
+    }
+
+    return null;
+  }
+
+  String _validatePassword(String password) {
+    if (password == null || password.isEmpty) {
+      return 'Password cannot be null.(l10n)';
+    }
+
+    if (password.length < 7 || password.length > 20) {
+      return 'Password lenght must be between 7 and 20.(l10n)';
+    }
+
+    var bufferPrefix = 'Password must contain(l10n)';
+    var buffer = StringBuffer();
+
+    if (!password.contains(RegExp(r'[A-Z]'))) {
+      buffer.write('\nat least one upper case letter(l10n)');
+    }
+
+    if (!password.contains(RegExp(r'[a-z]'))) {
+      buffer.write('\nat least one lower case letter(l10n)');
+    }
+
+    if (!password.contains(RegExp(r'[0-9]'))) {
+      buffer.write('\nat least one digit(l10n)');
+    }
+
+    if (!password.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+      buffer.write('\nat least one special character(l10n)');
+    }
+
+    return buffer.isNotEmpty ? bufferPrefix + buffer.toString() : null;
+  }
+
+  String _validatePasswordRepeat(String password) {
+    if (password != _passwordContoller.text) {
+      return 'Password doesn\'t match.(l10n)';
+    }
+
+    return null;
+  }
+
+  String _validateMail(String mail) {
+    if (mail == null || mail.isEmpty) {
+      return 'Mail cannot be null.(l10n)';
+    }
+
+    var pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+    if (!RegExp(pattern).hasMatch(mail)) {
+      return 'Invalid mail address.(l10n)';
     }
 
     return null;
---
+++ lib/widgets/textformfield.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class CustomTextFormField extends StatefulWidget {
+  final bool isObscureText;
+  final bool isRequired;
+  final String hintText;
+  final int errorLines;
+  final IconData iconData;
+  final Function validateFunc;
+  final TextEditingController controller;
+
+  CustomTextFormField({
+    Key key,
+    this.isObscureText = false,
+    this.isRequired = false,
+    this.errorLines = 2,
+    @required this.hintText,
+    this.iconData,
+    this.validateFunc,
+    @required this.controller,
+  }) : super(key: key);
+
+  @override
+  _CustomTextFormFieldState createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  Color _suffixIconColor = Colors.white;
+
+  @override
+  Widget build(BuildContext context) {
+    widget.controller.addListener(() {
+      setState(() {
+        _suffixIconColor = widget.validateFunc(widget.controller.text) == null
+            ? Colors.green
+            : Colors.white;
+      });
+    });
+    return TextFormField(
+      controller: widget.controller,
+      obscureText: widget.isObscureText,
+      validator: widget.validateFunc,
+      decoration: InputDecoration(
+        errorMaxLines: widget.errorLines,
+        helperText: widget.isRequired
+            ? AppLocalizations.of(context).required_field
+            : '',
+        hintText: widget.hintText,
+        icon: Icon(
+          this.widget.iconData,
+          size: 33,
+          color: Colors.grey,
+        ),
+        suffixIcon: Icon(
+          Icons.check,
+          color: _suffixIconColor,
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.grey,
+          ),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.pink,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    widget.controller.dispose();
+    super.dispose();
+  }
+}
