d415bb5fb84ec86026546c790009bcc888974d44
Halid Ali <halid.ali@methodpark.de>
2021-02-13 03:19:29 +0100

    visual changes in splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:guessGame/data/database.dart';
 import 'package:guessGame/data/models/user_model.dart';
+import 'package:guessGame/main.dart';
+import 'package:guessGame/screens/start_screen.dart';
 import 'package:guessGame/screens/user/register.dart';
 import 'package:guessGame/screens/user/user_list.dart';
 import 'package:guessGame/utils/constants.dart';
 import 'package:guessGame/widgets/app_bar.dart';
+import 'package:guessGame/widgets/button.dart';
 import 'package:guessGame/widgets/fade_transition.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -21,6 +24,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     _populateUsers();
+    AppLocalizations.delegate.load(Locale('de'));
   }
 
   @override
@@ -47,7 +51,7 @@ class _SplashScreenState extends State<SplashScreen> {
               Expanded(child: Container()),
               DropdownButton<User>(
                 value: _selectedUser,
-                hint: Text('Select User'),
+                hint: Text(AppLocalizations.of(context).select_user),
                 items: _users.map((User user) {
                   return DropdownMenuItem<User>(
                     value: user,
@@ -63,52 +67,98 @@ class _SplashScreenState extends State<SplashScreen> {
               SizedBox(height: 20),
               Container(
                 color: Colors.blue,
-                child: Text('Button is here'),
+                child: Text('Password box is here'),
               ),
+              SizedBox(height: 20),
+              CustomButton(
+                  color: Color(0xFF56CD4D),
+                  text: AppLocalizations.of(context).sign_in,
+                  func: () {
+                    Navigator.push(
+                      context,
+                      FadeRouteTransition(page: StartScreen()),
+                    );
+                  },
+                  icon: Icons.login_sharp),
               Expanded(child: Container()),
             ],
           ),
         ),
-        drawer: Drawer(
-          child: Container(
-            color: Colors.black,
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: ListView(
-                children: [
-                  DrawerHeader(
-                    margin: const EdgeInsets.only(bottom: 10),
-                    padding: const EdgeInsets.all(0),
-                    child: Container(
-                      color: Colors.grey[200],
-                      alignment: Alignment.center,
-                      child: Text('Logo Here'),
+        drawer: Container(
+          width: MediaQuery.of(context).size.width * 0.75,
+          child: Drawer(
+            child: Container(
+              color: Colors.black,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ListView(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            _getLanguageButton('EN', 'en'),
+                            Text('|', style: TextStyle(color: Colors.white)),
+                            _getLanguageButton('DE', 'de'),
+                            Text('|', style: TextStyle(color: Colors.white)),
+                            _getLanguageButton('TR', 'tr'),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                  getDivider(),
-                  getFlatMenu(AppLocalizations.of(context).sign_in, () {}),
-                  getDivider(),
-                  getFlatMenu(AppLocalizations.of(context).register, () {
-                    Navigator.push(
-                      context,
-                      FadeRouteTransition(
-                        page: RegisterScreen(),
+                    DrawerHeader(
+                      margin: const EdgeInsets.only(bottom: 10),
+                      padding: const EdgeInsets.all(0),
+                      child: Container(
+                        color: Colors.grey[200],
+                        alignment: Alignment.center,
+                        child: Text('Logo Here'),
                       ),
-                    );
-                  }),
-                  getDivider(),
-                  getFlatMenu(AppLocalizations.of(context).users, () {
-                    Navigator.push(
-                      context,
-                      FadeRouteTransition(
-                        page: UserListScreen(),
-                      ),
-                    );
-                  }),
-                  getDivider(),
-                  getFlatMenu(AppLocalizations.of(context).about, () {}),
-                  getDivider(),
-                ],
+                    ),
+                    _getDivider(),
+                    _getFlatMenu(
+                      AppLocalizations.of(context).settings,
+                      () {},
+                      Icons.settings_sharp,
+                    ),
+                    _getDivider(),
+                    _getFlatMenu(
+                      AppLocalizations.of(context).register,
+                      () {
+                        Navigator.push(
+                          context,
+                          FadeRouteTransition(
+                            page: RegisterScreen(),
+                          ),
+                        );
+                      },
+                      Icons.receipt_long_sharp,
+                    ),
+                    _getDivider(),
+                    _getFlatMenu(
+                      AppLocalizations.of(context).users,
+                      () {
+                        Navigator.push(
+                          context,
+                          FadeRouteTransition(
+                            page: UserListScreen(),
+                          ),
+                        );
+                      },
+                      Icons.person_sharp,
+                    ),
+                    _getDivider(),
+                    _getFlatMenu(
+                      AppLocalizations.of(context).about,
+                      () {},
+                      Icons.info_outlined,
+                    ),
+                    _getDivider(),
+                  ],
+                ),
               ),
             ),
           ),
@@ -124,7 +174,20 @@ class _SplashScreenState extends State<SplashScreen> {
     });
   }
 
-  Widget getDivider() {
+  Widget _getLanguageButton(String text, String countryCode) {
+    return TextButton(
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      onPressed: () => GameApp.of(context)
+          .setLocale(Locale.fromSubtags(languageCode: countryCode)),
+    );
+  }
+
+  Widget _getDivider() {
     return Divider(
       height: 5,
       thickness: 1,
@@ -132,19 +195,26 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 
-  Widget getFlatMenu(String title, Function func) {
+  Widget _getFlatMenu(String title, Function func, IconData icon) {
     return Container(
       child: FlatButton(
         padding: const EdgeInsets.all(5.0),
-        child: Container(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            title,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 14,
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
             ),
-          ),
+            Icon(icon, color: Colors.white),
+          ],
         ),
         onPressed: func,
       ),
