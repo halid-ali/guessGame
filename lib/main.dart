0a654b452237495b7dc203bb9c0e570a972a0390
Halid Ali <halid.ali@methodpark.de>
2021-02-13 03:18:33 +0100

    language change in app functionality

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,24 @@ void main() {
 class GameApp extends StatefulWidget {
   @override
   _GameAppState createState() => _GameAppState();
+
+  static _GameAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_GameAppState>();
 }
 
 class _GameAppState extends State<GameApp> {
-  bool isGameStarted = false;
+  Locale _locale;
+
+  void setLocale(Locale value) {
+    setState(() {
+      _locale = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: _locale,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       initialRoute: RouteGenerator.homePage,
