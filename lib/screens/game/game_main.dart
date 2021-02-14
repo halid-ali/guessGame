75577198ddf5c45736f8849b8cd6850cac809645
Halid Ali <halid.ali@methodpark.de>
2021-02-14 01:56:23 +0100

    init game main screen

---
+++ lib/screens/game/game_main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class GameMainScreen extends StatefulWidget {
+  GameMainScreen({Key key}) : super(key: key);
+
+  @override
+  _GameMainScreenState createState() => _GameMainScreenState();
+}
+
+class _GameMainScreenState extends State<GameMainScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _textController = TextEditingController();
+  final int _level = 9;
+  final bool _isZeroStartAllowed = false;
+  final bool _isSameDigitAllowed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Center(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Container(
+                    alignment: Alignment.topCenter,
+                    child: TextFormField(
+                      controller: _textController,
+                      style: TextStyle(fontSize: 33, letterSpacing: 5),
+                      textAlign: TextAlign.center,
+                      maxLength: 9,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.digitsOnly
+                      ],
+                      validator: _validateValue,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 9),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(0),
+                          ),
+                        ),
+                        hintText: '_________',
+                        errorStyle: TextStyle(fontSize: 11),
+                        errorMaxLines: 2,
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.send, color: Colors.pink),
+                          onPressed: () {
+                            if (_formKey.currentState.validate()) {
+                              print('Valid Number: ${_textController.text}');
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  String _validateValue(String value) {
+    if (value.length < _level) {
+      return 'You must enter $_level digits number.';
+    }
+
+    if (!_isZeroStartAllowed && value[0] == '0') {
+      return 'Zero (0) start isn\'t allowed.';
+    }
+
+    if (!_isSameDigitAllowed && _isContainsDuplicates(value)) {
+      return 'Duplicate digits aren\'t allowed.';
+    }
+
+    return null;
+  }
+
+  bool _isContainsDuplicates(String number) {
+    var charList = List<String>();
+
+    for (int i = 0; i < number.length; i++) {
+      var c = number.characters.elementAt(i);
+
+      if (charList.contains(c)) {
+        return true;
+      } else {
+        charList.add(c);
+      }
+    }
+
+    return false;
+  }
+}
