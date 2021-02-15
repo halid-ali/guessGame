76bead24780a09d26f9d9a0450519c33d101aa0f
Halid Ali <halid.ali@methodpark.de>
2021-02-15 06:27:38 +0100

    add sliding up panel in game screen

--- lib/screens/game/game_main.dart
+++ lib/screens/game/game_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:guessGame/generated/l10n.dart';
 
 class GameMainScreen extends StatefulWidget {
@@ -64,6 +65,41 @@ class _GameMainScreenState extends State<GameMainScreen> {
               ),
             ),
           ),
+        ),
+        floatingActionButton: SpeedDial(
+          animatedIcon: AnimatedIcons.menu_close,
+          animatedIconTheme: IconThemeData(size: 22),
+          backgroundColor: Color(0xFF56CD4D),
+          visible: true,
+          curve: Curves.bounceIn,
+          children: [
+            SpeedDialChild(
+              child: Icon(Icons.keyboard_arrow_right_sharp, size: 29),
+              backgroundColor: Colors.red[800],
+              onTap: () {
+                print('quit game');
+              },
+              label: S.of(context).quit,
+              labelStyle: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                  fontSize: 16.0),
+              labelBackgroundColor: Colors.red[800],
+            ),
+            SpeedDialChild(
+              child: Icon(Icons.help_outline_sharp, size: 29),
+              backgroundColor: Colors.pink,
+              onTap: () {
+                print('take a hint');
+              },
+              label: S.of(context).hint,
+              labelStyle: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                  fontSize: 16.0),
+              labelBackgroundColor: Colors.pink,
+            ),
+          ],
         ),
       ),
     );
